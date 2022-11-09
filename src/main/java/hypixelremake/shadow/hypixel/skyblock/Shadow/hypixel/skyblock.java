fbe86c23178e8bc5d8f9495a031fcb0c416b37eb
shadowlol2 <helloworldsa42@gmail.com>
2022-11-09 13:29:46 +0100

    The start

---
+++ src/main/java/hypixelremake/shadow/hypixel/skyblock/Shadow/hypixel/skyblock.java
@@ -0,0 +1,17 @@
+package hypixelremake.shadow.hypixel.skyblock.Shadow.hypixel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class skyblock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
